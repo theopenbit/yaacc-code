c3b8ca5d41ba37662f691bf617f5243da3b86e2f
TheRealEyeless <christoph+git@haehnelonline.de>
2013-01-04 22:14:47 +0100

    Added initial Activity for settings

--- main/src/de/yaacc/MainActivity.java
+++ main/src/de/yaacc/MainActivity.java
@@ -1,8 +1,11 @@
 package de.yaacc;
 
+import de.yaacc.config.SettingsActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -12,13 +15,23 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch(item.getItemId()){
+    	case R.id.menu_settings:
+    		Intent i = new Intent(this,SettingsActivity.class);
+    		startActivity(i);
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
     }
 }
---
+++ main/src/de/yaacc/config/SettingsActivity.java
@@ -0,0 +1,14 @@
+package de.yaacc.config;
+
+import de.yaacc.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
