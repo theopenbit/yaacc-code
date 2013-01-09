1de596ec7326dfb303b6f58e5c93904ff2d3d32e
TheRealEyeless <christoph+git@haehnelonline.de>
2013-01-09 17:21:14 +0100

    - updated german translation - changed theme to holo - refactored preferences to use PreferenceActivity instead of generic Activity - added generic devices in menu - added simple media filter options

--- main/src/de/yaacc/config/SettingsActivity.java
+++ main/src/de/yaacc/config/SettingsActivity.java
@@ -3,12 +3,14 @@ package de.yaacc.config;
 import de.yaacc.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity{
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.preference);
+        //setContentView(R.layout.activity_settings);
     }
 }
