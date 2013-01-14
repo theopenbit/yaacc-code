a6901dfb2ed943c42ce9f5e8a2f591bc2b88e75b
TheRealEyeless <christoph+git@haehnelonline.de>
2013-01-14 20:18:23 +0100

    Replaced the mocked device list with a real (but empty 0:) ) one.
    If this list contains some devices they should automatically be listed
    as receiver and sender

--- main/src/de/yaacc/config/SettingsActivity.java
+++ main/src/de/yaacc/config/SettingsActivity.java
@@ -1,6 +1,12 @@
 package de.yaacc.config;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.teleal.cling.model.meta.Device;
+
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import de.yaacc.R;
 
@@ -11,6 +17,37 @@ public class SettingsActivity extends PreferenceActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference);
+        
+
+        LinkedList<Device> devices = new LinkedList<Device>();
+        // TODO: populate with found devices
+        
+        
+        //Looks like this can not be connected via R.string.*
+        ListPreference providerLp = (ListPreference)findPreference("provider_list");
+        
+        // One entry per found device for providing media data
+        ArrayList<CharSequence> providerEntries = new ArrayList<CharSequence>();
+        ArrayList<CharSequence> providerEntryValues = new ArrayList<CharSequence>();
+        for(Device currentDevice: devices){
+        	providerEntries.add(currentDevice.getDisplayString());
+        	providerEntryValues.add(currentDevice.getIdentity().toString());
+        }
+        providerLp.setEntries(providerEntries.toArray(new CharSequence[providerEntries.size()]));
+        providerLp.setEntryValues(providerEntryValues.toArray(new CharSequence[providerEntries.size()]));
+        
+        
+        // One entry per found device for receiving media data
+        ListPreference receiverLp = (ListPreference)findPreference("receiver_list");
+        ArrayList<CharSequence> receiverEntries = new ArrayList<CharSequence>();
+        ArrayList<CharSequence> receiverEntryValues = new ArrayList<CharSequence>();
+        for(Device currentDevice: devices){
+        	receiverEntries.add(currentDevice.getDisplayString());
+        	receiverEntryValues.add(currentDevice.getIdentity().toString());
+        }
+        receiverLp.setEntries(receiverEntries.toArray(new CharSequence[receiverEntries.size()]));
+        receiverLp.setEntryValues(receiverEntryValues.toArray(new CharSequence[receiverEntries.size()]));
+        
         
 	}
 	
