3a9137e738922f98cdcf621ade143eb1153553a7
TheRealEyeless <christoph+git@haehnelonline.de>
2013-01-14 17:51:46 +0100

    settings fixed, they are stored automatically....

--- main/src/de/yaacc/config/SettingsActivity.java
+++ main/src/de/yaacc/config/SettingsActivity.java
@@ -1,16 +1,19 @@
 package de.yaacc.config;
 
-import de.yaacc.R;
-import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import de.yaacc.R;
 
 public class SettingsActivity extends PreferenceActivity{
+	
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference);
-        //setContentView(R.layout.activity_settings);
-    }
+        
+	}
+	
+	
+	
 }
