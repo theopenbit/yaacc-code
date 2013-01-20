e46af5154f64658c65b05079b1b26e79ea74ad35
TheRealEyeless <christoph+git@haehnelonline.de>
2013-01-20 15:57:41 +0100

    Listing now shows available devices ... global variables rule !

--- main/src/de/yaacc/config/SettingsActivity.java
+++ main/src/de/yaacc/config/SettingsActivity.java
@@ -2,13 +2,31 @@ package de.yaacc.config;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import org.teleal.cling.model.ValidationException;
+import org.teleal.cling.model.meta.Action;
 import org.teleal.cling.model.meta.Device;
+import org.teleal.cling.model.meta.DeviceDetails;
+import org.teleal.cling.model.meta.DeviceIdentity;
+import org.teleal.cling.model.meta.LocalDevice;
+import org.teleal.cling.model.meta.LocalService;
+import org.teleal.cling.model.meta.StateVariable;
+import org.teleal.cling.model.meta.StateVariableTypeDetails;
+import org.teleal.cling.model.types.ServiceId;
+import org.teleal.cling.model.types.ServiceType;
+import org.teleal.cling.model.types.StringDatatype;
+import org.teleal.cling.model.types.UDADeviceType;
+import org.teleal.cling.model.types.UDN;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import de.yaacc.MainActivity;
 import de.yaacc.R;
+import de.yaacc.upnp.UpnpClient;
 
 public class SettingsActivity extends PreferenceActivity{
 	
@@ -16,12 +34,20 @@ public class SettingsActivity extends PreferenceActivity{
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preference);
+        addPreferencesFromResource(R.layout.preference);
         
 
         LinkedList<Device> devices = new LinkedList<Device>();
         // TODO: populate with found devices
-        
+		
+       UpnpClient upnpClient = MainActivity.uClient;
+        	
+        if (upnpClient.isInitialized()){
+
+	        devices.addAll(upnpClient.getDevices());
+		}
+
+		
         
         //Looks like this can not be connected via R.string.*
         ListPreference providerLp = (ListPreference)findPreference("provider_list");
