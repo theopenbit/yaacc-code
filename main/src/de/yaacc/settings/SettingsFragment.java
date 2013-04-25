82d963483c3caf5288970ec5bb5e4c43a04986e0
therealeyeless <christoph@haehnelonline.de>
2013-04-25 21:56:43 +0200

    - converted settings from PreferenceActivity to PreferenceFragment, to avoid deprecated code

---
+++ main/src/de/yaacc/settings/SettingsFragment.java
@@ -0,0 +1,83 @@
+package de.yaacc.settings;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.teleal.cling.model.meta.Device;
+
+import de.yaacc.R;
+import de.yaacc.browser.BrowseActivity;
+import de.yaacc.upnp.UpnpClient;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		addPreferencesFromResource(R.xml.preference);
+		
+	
+
+		//populateDeviceLists();
+	}
+	
+	public void addDevice(){
+		
+	}
+
+	private void populateDeviceLists() {
+		LinkedList<Device> devices = new LinkedList<Device>();
+		// TODO: populate with found devices
+
+		UpnpClient upnpClient = BrowseActivity.uClient;
+
+		if (upnpClient != null) {
+			if (upnpClient.isInitialized()) {
+				devices.addAll(upnpClient
+						.getDevicesProvidingContentDirectoryService());
+			}
+
+			ListPreference providerLp = (ListPreference) findPreference(getString(R.string.settings_selected_provider_title));
+
+			// One entry per found device for providing media data
+			ArrayList<CharSequence> providerEntries = new ArrayList<CharSequence>();
+			ArrayList<CharSequence> providerEntryValues = new ArrayList<CharSequence>();
+			for (Device currentDevice : devices) {
+				providerEntries.add(currentDevice.getDisplayString());
+				providerEntryValues.add(currentDevice.getIdentity().getUdn()
+						.getIdentifierString());
+			}
+
+			providerLp.setEntries(providerEntries
+					.toArray(new CharSequence[providerEntries.size()]));
+			providerLp.setEntryValues(providerEntryValues
+					.toArray(new CharSequence[providerEntries.size()]));
+
+			devices = new LinkedList<Device>();
+			devices.addAll(upnpClient.getDevicesProvidingAvTransportService());
+
+			// One entry per found device for receiving media data
+			ListPreference receiverLp = (ListPreference) findPreference(getString(R.string.settings_selected_receiver_title));
+			ArrayList<CharSequence> receiverEntries = new ArrayList<CharSequence>();
+			ArrayList<CharSequence> receiverEntryValues = new ArrayList<CharSequence>();
+			for (Device currentDevice : devices) {
+				receiverEntries.add(currentDevice.getDisplayString());
+				receiverEntryValues.add(currentDevice.getIdentity().getUdn()
+						.getIdentifierString());
+			}
+
+			// Add a default entry for the local device
+			receiverEntries.add(android.os.Build.MODEL);
+			receiverEntryValues.add(UpnpClient.LOCAL_UID);
+
+			receiverLp.setEntries(receiverEntries
+					.toArray(new CharSequence[receiverEntries.size()]));
+			receiverLp.setEntryValues(receiverEntryValues
+					.toArray(new CharSequence[receiverEntries.size()]));
+		}
+	}
+
+}
