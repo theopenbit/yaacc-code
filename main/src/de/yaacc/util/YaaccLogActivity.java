8ce0ad8f20114713a925398308305c050bc1c54e
TheOpenBit <tob@schoenesnetz.de>
2014-09-04 20:26:27 +0000

    ticket #94 added in app logviewer

---
+++ main/src/de/yaacc/util/YaaccLogActivity.java
@@ -0,0 +1,55 @@
+package de.yaacc.util;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import de.yaacc.R;
+
+public class YaaccLogActivity extends Activity {
+    public static void showLog(Activity activity) {
+        activity.startActivity(new Intent(activity, YaaccLogActivity.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yaacc_log);
+
+
+        TextView textView = (TextView) findViewById(R.id.yaaccLog_content);
+
+        try {
+            Process process = Runtime.getRuntime().exec("logcat -d");
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            StringBuilder log = new StringBuilder();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+                log.append("\n");
+            }
+
+            textView.setText(log.toString());
+        } catch (IOException e) {
+            textView.setText("Error while reading log: " + e.getMessage());
+        }
+    }
+
+
+
+}
